697a9d2bb8a535395ad1238815ba3b00cf0f7d27
Dave O <infodave212@gmail.com>
2024-04-05 23:21:05 -0500

    add category route

--- Develop/models/Tag.js
+++ Develop/models/Tag.js
@@ -8,37 +8,14 @@ Tag.init(
   {
     // define columns
     id:{
-      type: DataType.INTEGER,
-      allowNul: false,
+      type: DataTypes.INTEGER,
+      allowNull: false,
       primaryKey: true,
       autoIncrement: true,
     },
     tag_name: {
       type: DataTypes.STRING,
     },
-    productTag: {
-      id: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        primaryKey: true,
-        autoIncrement: true,
-        },
-        product_id: {
-          type: DataTypes.INTEGER,
-          references: {
-            model: 'product',
-            key: 'id',
-          },
-          tag_id: {
-            type: DataTypes.INTEGER,
-            references: {
-              model: 'tag',
-              key: 'id',
-            }
-          }
-        }
-      }
-    }
 
   },
   {
--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -2,27 +2,79 @@ const router = require('express').Router();
 const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
-
-router.get('/', (req, res) => {
-  // find all categories
-  // be sure to include its associated Products
+//http://localhost:3001/api/categories/
+router.get('/', async (req, res) => {
+  try {
+    const categoryData = await Category.findAll({
+      // Add Book as a second model to JOIN with
+      include: [{ model: Product}],
+    });
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
+//http://localhost:3001/api/categories/1
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  router.get('/', async (req, res) => {
+    try {
+      const categoryData = await Product.findAll({
+        // Add Book as a second model to JOIN with
+        include: [{ model: Product }],
+      });
+      res.status(200).json(categoryData);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
 });
-
+//http://localhost:3001/api/categories
 router.post('/', (req, res) => {
   // create a new category
 });
-
+router.get('/', async (req, res) => {
+  try {
+    const categoryData = await Category.findAll({
+      // Add Book as a second model to JOIN with
+      include: [{ model: Product }],
+    });
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+//http://localhost:3001/api/categories/2
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
 });
-
+router.get('/', async (req, res) => {
+  try {
+    const categoryData = await Category.findAll({
+      // Add Book as a second model to JOIN with
+      include: [{ model: Product }],
+    });
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+//http://localhost:3001/api/categories/3
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
+});
+router.get('/', async (req, res) => {
+  try {
+    const categoryData = await Category.findAll({
+      // Add Book as a second model to JOIN with
+      include: [{ model: Product }],
+    });
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
