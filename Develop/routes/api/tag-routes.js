6b00fc67ff69c4b1b32ad20a61c6763f0d1634b2
Dave O <infodave212@gmail.com>
2024-04-17 23:31:36 -0500

    added tag in the insomnia

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -33,8 +33,8 @@ router.get('/:id', async (req, res) => {
 });
 router.post('/', async (req, res) => {
   try {
-    const tagData = await Tag.create(req.body);
-    res.status(200).json(tagnData);
+    const tagsData = await Tag.create(req.body);
+    res.status(200).json(tagsData);
   } catch (err) {
     res.status(400).json(err);
   }
